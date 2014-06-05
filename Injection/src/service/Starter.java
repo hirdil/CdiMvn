6f2ed4d193336ff9ace7d36697f82e915d826f56
hirdil <laszlohirdi@gmail.com>
2014-06-05 22:00:17 +0200

    final

--- Injection/src/service/Starter.java
+++ Injection/src/service/Starter.java
@@ -3,6 +3,7 @@ package service;
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -14,7 +15,7 @@ import data.Plant;
 @Startup
 public class Starter {
 
-	// field injection
+	// 1. field injection
 	@Inject
 	private Apple _apple;
 
@@ -23,25 +24,32 @@ public class Starter {
 		System.out.println("_apple = " + _apple);
 	}
 
-	// constructor injection
+	// 2. constructor injection
 	@Inject
 	public Starter(@Named("apple") final Fruit fruit2,
 			@Named("plum") final Plant fruit3) {
 		System.out.println("constructor injection");
 		System.out.println("fruit2 = " + fruit2);
 		System.out.println("fruit3 = " + fruit3);
+		System.out.println("_appleField = " + _apple);
 	}
 
-	// method (setter) injection
+	// 3. method (setter) injection
 	@Inject
 	public void setter(final Apple apple2) {
 		System.out.println("method");
 		System.out.println("apple2 = " + apple2);
 	}
 
+	// 4. dynamic injection
+	// it can be inside method bodies to create cdi beans dynamically.
+	@Inject
+	protected Instance<Apple> _dynamicApple;
+	
 	@PostConstruct
 	private void init() {
 		System.out.println("init");
 		System.out.println("_apple = " + _apple);
+		System.out.println("get a dynamic Apple = " + _dynamicApple.get());
 	}
 }
